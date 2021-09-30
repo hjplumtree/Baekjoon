102747c2fd17fa52c94b0d49bca19859477ca846
HeJ <dark199174@gmail.com>
2021-09-30 15:47:14 +0900

    10870 done

---
+++ 10870.js
@@ -0,0 +1,10 @@
+const input = +require('fs').readFileSync('/dev/stdin')
+function fibonacci(n) {
+  if(n <= 1) {
+    return n
+  }
+  else {
+    return fibonacci(n - 1) + fibonacci(n - 2)
+  }
+}
+console.log(fibonacci(input));
