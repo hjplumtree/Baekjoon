68cb391034f9c9334a990d51f680aa12f42d458f
Hee Jae <dark199174@gmail.com>
2021-10-01 11:04:32 +0900

    2751 merge sort way

--- 2751.js
+++ 2751.js
@@ -1,21 +1,37 @@
 const fs = require('fs')
 let input = fs.readFileSync('2751.txt').toString().split('\n')
 
-let total = +input.shift()
-let numbers = input.map(Number)
-// let order = numbers.splice(0, 1)
+let [N, ...order] = input.map(Number)
+order = mergeSort(order)
+function mergeSort(arr) {
+    if(arr.length < 2) {
+        return arr
+    }
 
-// for(let i = 0; i < total - 1; i++) {
-//   let temp = order.length
-//   for(let j = 0; j < temp; j++) {
-//     if(order[j] > numbers[i]) {
-//       order = [...order.slice(0, j), numbers[i], ...order.slice(j)]
-//       break;
-//     }
-//     if(j === temp - 1) {
-//       order.push(numbers[i])
-//     }
-//   }
-// }
-numbers = numbers.sort((a, b) => a - b)
-numbers.forEach(ele => console.log(ele))
+    let mid = Math.floor(arr.length / 2)
+    let left = arr.slice(0, mid)
+    let right = arr.slice(mid)
+
+    return merge(mergeSort(left), mergeSort(right))
+}
+
+function merge(left, right) {
+    let result = []
+
+    let l_index = 0
+    let r_index = 0
+
+    while (l_index < left.length && r_index < right.length){
+        if(left[l_index] < right[r_index]){
+            result.push(left[l_index])
+            l_index++
+        }
+        else {
+            result.push(right[r_index])
+            r_index++
+        }
+    }
+    return [...result, ...left.slice(l_index), ...right.slice(r_index)]
+}
+
+console.log(order.join('\n'))
